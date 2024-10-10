659fa6133ace7243da6db9bf9419d99a5063c020
Felipe Anony <felipemom2001@gmail.com>
2024-10-11 00:11:06 +0200

    refactor: move files to lib folder

---
+++ lib/main.ts
@@ -0,0 +1,4 @@
+import useScreenRecorder from "./use-screen-recorder";
+import useHttpRecorder from "./use-requests-recorder";
+
+export { useScreenRecorder, useHttpRecorder };
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./use-screen-recorder";
-export * from "./use-requests-recorder";
