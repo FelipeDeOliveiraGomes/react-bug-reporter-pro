9881d0e266fafa74031fca47298bde6788c2fae7
Felipe Anony <felipemom2001@gmail.com>
2024-10-11 01:21:35 +0200

    refactor: format files

--- lib/use-requests-recorder.ts
+++ lib/use-requests-recorder.ts
@@ -1,57 +1,59 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 import {
-  BrowserHttpRequestListener,
-  RequestResponseModel,
-} from "browser-http-request-listener";
+    BrowserHttpRequestListener,
+    RequestResponseModel,
+} from 'browser-http-request-listener'
 
 interface UseHttpRecorderReturn {
-  recording: boolean;
-  startRecording: () => void;
-  stopRecording: () => void;
+    recording: boolean
+    startRecording: () => void
+    stopRecording: () => void
 }
 
 function useHttpRecorder(): UseHttpRecorderReturn {
-  const [recording, setRecording] = useState(false);
-  const [requests, setRequests] = useState<RequestResponseModel[]>([]);
+    const [recording, setRecording] = useState(false)
+    const [requests, setRequests] = useState<RequestResponseModel[]>([])
 
-  const startRecording = () => {
-    setRequests([]);
-    BrowserHttpRequestListener.start();
-    setRecording(true);
-  };
-
-  const stopRecording = () => {
-    BrowserHttpRequestListener.stop();
-    setRecording(false);
-
-    if (requests.length > 0) {
-      const fileContent = requests.map((req) => JSON.stringify(req)).join("\n");
-      const blob = new Blob([fileContent], { type: "text/plain" });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.style.display = "none";
-      a.href = url;
-      a.download = "http-requests-log.txt";
-      document.body.appendChild(a);
-      a.click();
-      window.URL.revokeObjectURL(url);
+    const startRecording = () => {
+        setRequests([])
+        BrowserHttpRequestListener.start()
+        setRecording(true)
     }
-  };
 
-  useEffect(() => {
-    const httpReqListenerUnsubscriber =
-      BrowserHttpRequestListener.onHttpResponseArrives((req) => {
-        setRequests((prev) => [...prev, req]);
-      });
+    const stopRecording = () => {
+        BrowserHttpRequestListener.stop()
+        setRecording(false)
 
-    return httpReqListenerUnsubscriber;
-  }, []);
+        if (requests.length > 0) {
+            const fileContent = requests
+                .map((req) => JSON.stringify(req))
+                .join('\n')
+            const blob = new Blob([fileContent], { type: 'text/plain' })
+            const url = URL.createObjectURL(blob)
+            const a = document.createElement('a')
+            a.style.display = 'none'
+            a.href = url
+            a.download = 'http-requests-log.txt'
+            document.body.appendChild(a)
+            a.click()
+            window.URL.revokeObjectURL(url)
+        }
+    }
 
-  return {
-    recording,
-    startRecording,
-    stopRecording,
-  };
+    useEffect(() => {
+        const httpReqListenerUnsubscriber =
+            BrowserHttpRequestListener.onHttpResponseArrives((req) => {
+                setRequests((prev) => [...prev, req])
+            })
+
+        return httpReqListenerUnsubscriber
+    }, [])
+
+    return {
+        recording,
+        startRecording,
+        stopRecording,
+    }
 }
 
-export default useHttpRecorder;
+export default useHttpRecorder
