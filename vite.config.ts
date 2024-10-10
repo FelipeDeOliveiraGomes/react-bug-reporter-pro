c8507c1a714f510a362424d416b536153b9f7b74
Felipe Anony <felipemom2001@gmail.com>
2024-10-10 21:54:04 +0200

    chore: config vite to produce lib code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "ReactProBugReporter",
+      formats: ["es", "umd"],
+      fileName: (format) => `react-pro-bug-reporter.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
