0bf519da0232d6d058b93bb43991a248bd6228bd
Felipe Anony <felipemom2001@gmail.com>
2024-10-11 01:16:44 +0200

    fix: solve build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import dts from "vite-plugin-dts";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
-      name: "ReactProBugReporter",
-      formats: ["es"],
-      fileName: "react-pro-bug-reporter",
+      entry: resolve(__dirname, "./lib/index.ts"),
+      name: "react-bug-reporter-pro",
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
@@ -26,5 +20,8 @@ export default defineConfig({
         },
       },
     },
+    sourcemap: true,
+    emptyOutDir: true,
   },
+  plugins: [react(), dts({ rollupTypes: true })],
 });
