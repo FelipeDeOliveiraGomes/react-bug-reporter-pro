047d268f05ee75b18dd49e4e6e699e8c9df2666c
Felipe Anony <felipemom2001@gmail.com>
2024-10-12 20:27:06 +0200

    chore: adds inject css in js vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,29 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "./lib/index.ts"),
-      name: "react-bug-reporter-pro",
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+    build: {
+        lib: {
+            entry: resolve(__dirname, './lib/index.ts'),
+            name: 'react-bug-reporter-pro',
+            fileName: (format) => `index.${format}.js`,
         },
-      },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
+        sourcemap: true,
+        emptyOutDir: true,
+        cssCodeSplit: true,
     },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
-  plugins: [react(), dts({ rollupTypes: true })],
-});
+    plugins: [react(), dts({ rollupTypes: true }), cssInjectedByJsPlugin()],
+})
