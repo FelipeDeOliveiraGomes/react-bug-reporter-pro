16fc921724ef7f3801ea7a248d03d3422ee96f0d
Felipe Anony <felipemom2001@gmail.com>
2024-10-12 15:44:11 +0200

    fix: adds missing css import in lib index file

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import useScreenRecorder from './hooks/use-screen-recorder'
 import useHttpRecorder from './hooks/use-requests-recorder'
 import ReactBugReporterProWrapper from './react-bug-reporter-pro-wrapper/react-bug-reporter-pro-wrapper'
+import './react-bug-reporter-pro-wrapper/react-bug-reporter-pro-wrapper.css'
 
 export { useScreenRecorder, useHttpRecorder, ReactBugReporterProWrapper }
